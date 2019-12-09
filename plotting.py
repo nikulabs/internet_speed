4e09b3219cce35b2093cea4d109f57eda7877c14
Ben Nikula <nikulabs@gmail.com>
2019-12-09 22:13:44 +0000

    Plot ping as well as download speed.

--- plotting.py
+++ plotting.py
@@ -1,17 +1,21 @@
 import pandas as pd
-import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 df = pd.read_csv('/home/brn/speedtest.csv')
 
-#fig = make_subplots(specs=[[{"secondary_y": True}]])
-#fig.add_trace(go.Scatter(x = df['Timestamp'], y = df['Download'],
-#                  name='Download speed over time'))
+fig = make_subplots(specs=[[{"secondary_y": True}]])
+fig.add_trace(go.Scatter(x = df['Timestamp'], y = df['Download'],
+                  name='Download (bps)'), secondary_y=False)
+fig.add_trace(go.Scatter(x = df['Timestamp'], y = df['Ping'],
+                  name='Ping (msec)'), secondary_y=True)
 
-#fig.update_layout(title='Apple Share Prices over time (2014)',
-#                   plot_bgcolor='rgb(230, 230,230)',
-#                   showlegend=True)
+fig.update_layout(title='Vodafone home internet connection',
+                   plot_bgcolor='rgb(230, 230,230)',
+                   showlegend=True)
 
-#fig.show()
-fig = px.line(df, x = 'Timestamp', y = 'Download', title='Download speed over time(Dec 2019)')
+fig.update_xaxes(title_text="Time")
+fig.update_yaxes(title_text="Speed", secondary_y=False)
+fig.update_yaxes(title_text="Ping", secondary_y=True)
+
 fig.show()
